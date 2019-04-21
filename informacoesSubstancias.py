ecdd1b2723d0c08f8663d19e7cd96a11bc407482
gabrielvf1 <vf_gabriel@yahoo.com.br>
2019-04-21 20:36:32 -0300

    dicionario subatancias

---
+++ informacoesSubstancias.py
@@ -0,0 +1,241 @@
+litio = {}
+
+litio['EOS'] = [1,'Li']
+litio['EOE'] = [1, 'e']
+litio['ER'] = ['Li']
+litio['PO'] =  3.04
+litio['PR'] = litio['PO']*-1
+
+potassio = {}
+
+potassio['EOS'] = [1,'K']
+potassio['EOE'] = [1, 'e']
+potassio['ER'] = ['K']
+potassio['PO'] =  2.92
+potassio['PR'] = potassio['PO']*-1
+
+bario = {}
+
+bario['EOS'] = [1,'Ba']
+bario['EOE'] = [2, 'e']
+bario['ER'] = ['Ba']
+bario['PO'] =  2.90
+bario['PR'] = bario['PO']*-1
+
+estroncio = {}
+
+estroncio['EOS'] = [1,'Sr']
+estroncio['EOE'] = [2, 'e']
+estroncio['ER'] = ['Sr']
+estroncio['PO'] =  2.89
+estroncio['PR'] = estroncio['PO']*-1
+
+calcio = {}
+
+calcio['EOS'] = [1,'Ca']
+calcio['EOE'] = [2, 'e']
+calcio['ER'] = [1,'Ca']
+calcio['PO'] =  2.87
+calcio['PR'] = calcio['PO']*-1
+
+sodio = {}
+
+sodio['EOS'] = [1,'Na']
+sodio['EOE'] = [1, 'e']
+sodio['ER'] = [1,'Na']
+sodio['PO'] =  2.71
+sodio['PR'] = sodio['PO']*-
+
+magnesio = {}
+
+magnesio['EOS'] = [1,'Mg']
+magnesio['EOE'] = [2, 'e']
+magnesio['ER'] = [1,'Mg']
+magnesio['PO'] =  2.37
+magnesio['PR'] = magnesio['PO']*-1
+
+aluminio = {}
+
+aluminio['EOS'] = [1,'Al']
+aluminio['EOE'] = [3, 'e']
+aluminio['ER'] = [1,'Al']
+aluminio['PO'] =  1.66
+aluminio['PR'] = aluminio['PO']*-1
+
+manganes = {}
+
+manganes['EOS'] = [1,'Mn']
+manganes['EOE'] = [2, 'e']
+manganes['ER'] = [1,'Mn']
+manganes['PO'] =  1.18
+manganes['PR'] = manganes['PO']*-1
+
+H22OH = {}
+
+H22OH['EOS'] = [2,'H20']
+H22OH['EOE'] = [2, 'e']
+H22OH['ER'] = [2,'H',2,'OH']
+H22OH['PO'] =  0.83
+H22OH['PR'] = H22OH['PO']*-1
+
+zinco = {}
+
+zinco['EOS'] = [1,'Zn']
+zinco['EOE'] = [2, 'e']
+zinco['ER'] = [1,'Zn']
+zinco['PO'] =  0.76
+zinco['PR'] = zinco['PO']*-1
+
+cromio = {}
+
+cromio['EOS'] = [1,'Cr']
+cromio['EOE'] = [3, 'e']
+cromio['ER'] = [1,'Cr']
+cromio['PO'] =  0.74
+cromio['PR'] = cromio['PO']*-1
+
+enxofre = {}
+
+enxofre['EOS'] = [1,'S']
+enxofre['EOE'] = [2, 'e']
+enxofre['ER'] = [1,'S']
+enxofre['PO'] =  0.48
+enxofre['PR'] = enxofre['PO']*-1
+
+ferro = {}
+
+ferro['EOS'] = [1,'Fe']
+ferro['EOE'] = [2, 'e']
+ferro['ER'] = [1,'Fe']
+ferro['PO'] =  0.44
+ferro['PR'] = ferro['PO']*-1
+
+cobalto = {}
+
+cobalto['EOS'] = [1,'Co']
+cobalto['EOE'] = [2, 'e']
+cobalto['ER'] = [1,'Co']
+cobalto['PO'] =  0.28
+cobalto['PR'] = cobalto['PO']*-1
+
+niquel = {}
+
+niquel['EOS'] = [1,'Ni']
+niquel['EOE'] = [2, 'e']
+niquel['ER'] = [1,'Ni']
+niquel['PO'] =  0.23
+niquel['PR'] = niquel['PO']*-1
+
+chumbo = {}
+
+chumbo['EOS'] = [1,'Pb']
+chumbo['EOE'] = [2, 'e']
+chumbo['ER'] = [1,'Pb']
+chumbo['PO'] =  0.13
+chumbo['PR'] = chumbo['PO']*-1
+
+hidrogenio = {}
+
+hidrogenio['EOS'] = [2,'H']
+hidrogenio['EOE'] = [2, 'e']
+hidrogenio['ER'] = [2,'H']
+hidrogenio['PO'] =  0.0
+hidrogenio['PR'] = hidrogenio['PO']*-1
+
+cobre1 = {}
+
+cobre1['EOS'] = [1,'Cu']
+cobre1['EOE'] = [1, 'e']
+cobre1['ER'] = [1,'Cu']
+cobre1['PO'] =  -0.15
+cobre1['PR'] = cobre1['PO']*-1
+
+cobre = {}
+
+cobre['EOS'] = [1,'Cu']
+cobre['EOE'] = [2, 'e']
+cobre['ER'] = [1,'Cu']
+cobre['PO'] =  -0.34
+cobre['PR'] = cobre['PO']*-1
+
+2OH = {}
+
+2OH['EOS'] = [1,'H20',1/2,'O2']
+2OH['EOE'] = [2, 'e']
+2OH['ER'] = [2,'OH']
+2OH['PO'] =  -0.40
+2OH['PR'] = 2OH['PO']*-1
+
+cobre0 = {}
+
+cobre0['EOS'] = [1,'Cu']
+cobre0['EOE'] = [1, 'e']
+cobre0['ER'] = [1,'Cu']
+cobre0['PO'] =  -0.52
+cobre0['PR'] = cobre0['PO']*-1
+
+iodo = {}
+
+iodo['EOS'] = [2,'I']
+iodo['EOE'] = [2, 'e']
+iodo['ER'] = [2,'I']
+iodo['PO'] =  -0.54
+iodo['PR'] = iodo['PO']*-1
+
+ferro2 = {}
+
+ferro2['EOS'] = [1,'Fe']
+ferro2['EOE'] = [1, 'e']
+ferro2['ER'] = [1,'Fe']
+ferro2['PO'] =  -0.77
+ferro2['PR'] = ferro2['PO']*-1
+
+prata = {}
+
+prata['EOS'] = [1,'Ag']
+prata['EOE'] = [1, 'e']
+prata['ER'] = [1,'Ag']
+prata['PO'] =  -0.80
+prata['PR'] = prata['PO']*-1
+
+mercurio = {}
+
+mercurio['EOS'] = [1,'Hg']
+mercurio['EOE'] = [2, 'e']
+mercurio['ER'] = [1,'Hg']
+mercurio['PO'] =  -0.85
+mercurio['PR'] = mercurio['PO']*-1
+
+bromo = {}
+
+bromo['EOS'] = [2,'Br']
+bromo['EOE'] = [2, 'e']
+bromo['ER'] = [2,'Br']
+bromo['PO'] =  -1.09
+bromo['PR'] = bromo['PO']*-1
+
+agua = {}
+
+agua['EOS'] = [2,'H',1/2,'O2']
+agua['EOE'] = [2, 'e']
+agua['ER'] = [1,'H2O']
+agua['PO'] =  -1.23
+agua['PR'] = agua['PO']*-1
+
+cloro = {}
+
+cloro['EOS'] = [2,'Cl']
+cloro['EOE'] = [2, 'e']
+cloro['ER'] = [2,'Cl']
+cloro['PO'] =  -1.36
+cloro['PR'] = cloro['PO']*-1
+
+fluor = {}
+
+fluor['EOS'] = [2,'F']
+fluor['EOE'] = [2, 'e']
+fluor['ER'] = [2,'F']
+fluor['PO'] =  -2.87
+fluor['PR'] = fluor['PO']*-1
+
+
