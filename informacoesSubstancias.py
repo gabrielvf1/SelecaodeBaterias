7745581fe144bd7d437985c6b7341be92eff3354
Samuelgranato <samuelgranato87@gmail.com>
2019-04-22 16:52:59 -0300

    Html files

--- informacoesSubstancias.py
+++ informacoesSubstancias.py
@@ -1,272 +1,247 @@
-litio = {}
-
-litio['EOS'] = [1,'Li']
-litio['EOE'] = [1, 'e']
-litio['ER'] = ['Li']
-litio['PO'] =  3.04
-litio['PR'] = litio['PO']*-1
-litio['PRECO'] =  190 * 4
-
-potassio = {}
-
-potassio['EOS'] = [1,'K']
-potassio['EOE'] = [1, 'e']
-potassio['ER'] = ['K']
-potassio['PO'] =  2.92
-potassio['PR'] = potassio['PO']*-1
-potassio['PRECO'] =  20 * 4
-
-bario = {}
-
-bario['EOS'] = [1,'Ba']
-bario['EOE'] = [2, 'e']
-bario['ER'] = ['Ba']
-bario['PO'] =  2.90
-bario['PR'] = bario['PO']*-1
-bario['PRECO'] =  120 * 4
-
-estroncio = {}
-
-estroncio['EOS'] = [1,'Sr']
-estroncio['EOE'] = [2, 'e']
-estroncio['ER'] = ['Sr']
-estroncio['PO'] =  2.89
-estroncio['PR'] = estroncio['PO']*-1
-estroncio['PRECO'] =  5.5 * 4
-
-calcio = {}
-
-calcio['EOS'] = [1,'Ca']
-calcio['EOE'] = [2, 'e']
-calcio['ER'] = [1,'Ca']
-calcio['PO'] =  2.87
-calcio['PR'] = calcio['PO']*-1
-calcio['PRECO'] =  2 * 4
-
-sodio = {}
-
-sodio['EOS'] = [1,'Na']
-sodio['EOE'] = [1, 'e']
-sodio['ER'] = [1,'Na']
-sodio['PO'] =  2.71
-sodio['PR'] = sodio['PO']*-1
-sodio['PRECO'] =  15 * 4
-
-magnesio = {}
-
-magnesio['EOS'] = [1,'Mg']
-magnesio['EOE'] = [2, 'e']
-magnesio['ER'] = [1,'Mg']
-magnesio['PO'] =  2.37
-magnesio['PR'] = magnesio['PO']*-1
-magnesio['PRECO'] =  14 * 4
-
-aluminio = {}
-
-aluminio['EOS'] = [1,'Al']
-aluminio['EOE'] = [3, 'e']
-aluminio['ER'] = [1,'Al']
-aluminio['PO'] =  1.66
-aluminio['PR'] = aluminio['PO']*-1
-aluminio['PRECO'] =  3.5 * 4
-
-manganes = {}
-
-manganes['EOS'] = [1,'Mn']
-manganes['EOE'] = [2, 'e']
-manganes['ER'] = [1,'Mn']
-manganes['PO'] =  1.18
-manganes['PR'] = manganes['PO']*-1
-manganes['PRECO'] =  1.8 * 4
-
-H22OH = {}
-
-H22OH['EOS'] = [2,'H20']
-H22OH['EOE'] = [2, 'e']
-H22OH['ER'] = [2,'H',2,'OH']
-H22OH['PO'] =  0.83
-H22OH['PR'] = H22OH['PO']*-1
-H22OH['PRECO'] =  1 * 4
-
-zinco = {}
-
-zinco['EOS'] = [1,'Zn']
-zinco['EOE'] = [2, 'e']
-zinco['ER'] = [1,'Zn']
-zinco['PO'] =  0.76
-zinco['PR'] = zinco['PO']*-1
-zinco['PRECO'] =  2.85 * 4
-
-cromio = {}
-
-cromio['EOS'] = [1,'Cr']
-cromio['EOE'] = [3, 'e']
-cromio['ER'] = [1,'Cr']
-cromio['PO'] =  0.74
-cromio['PR'] = cromio['PO']*-1
-cromio['PRECO'] =  16 * 4 
-
-enxofre = {}
-
-enxofre['EOS'] = [1,'S']
-enxofre['EOE'] = [2, 'e']
-enxofre['ER'] = [1,'S']
-enxofre['PO'] =  0.48
-enxofre['PR'] = enxofre['PO']*-1
-enxofre['PRECO'] =  17 * 4 
-
-ferro = {}
-
-ferro['EOS'] = [1,'Fe']
-ferro['EOE'] = [2, 'e']
-ferro['ER'] = [1,'Fe']
-ferro['PO'] =  0.44
-ferro['PR'] = ferro['PO']*-1
-ferro['PRECO'] =  2 * 4 
-
-cobalto = {}
-
-cobalto['EOS'] = [1,'Co']
-cobalto['EOE'] = [2, 'e']
-cobalto['ER'] = [1,'Co']
-cobalto['PO'] =  0.28
-cobalto['PR'] = cobalto['PO']*-1
-cobalto['PRECO'] =  80 * 4 
-
-niquel = {}
-
-niquel['EOS'] = [1,'Ni']
-niquel['EOE'] = [2, 'e']
-niquel['ER'] = [1,'Ni']
-niquel['PO'] =  0.23
-niquel['PR'] = niquel['PO']*-1
-niquel['PRECO'] =  13 * 4 
-
-chumbo = {}
-
-chumbo['EOS'] = [1,'Pb']
-chumbo['EOE'] = [2, 'e']
-chumbo['ER'] = [1,'Pb']
-chumbo['PO'] =  0.13
-chumbo['PR'] = chumbo['PO']*-1
-chumbo['PRECO'] =  2 * 4 
-
-hidrogenio = {}
-
-hidrogenio['EOS'] = [2,'H']
-hidrogenio['EOE'] = [2, 'e']
-hidrogenio['ER'] = [2,'H']
-hidrogenio['PO'] =  0.0
-hidrogenio['PR'] = hidrogenio['PO']*-1
-hidrogenio['PRECO'] =  10 * 4 
-
-cobre1 = {}
-
-cobre1['EOS'] = [1,'Cu']
-cobre1['EOE'] = [1, 'e']
-cobre1['ER'] = [1,'Cu']
-cobre1['PO'] =  -0.15
-cobre1['PR'] = cobre1['PO']*-1
-cobre1['PRECO'] =  6.5 * 4 
-
-cobre = {}
-
-cobre['EOS'] = [1,'Cu']
-cobre['EOE'] = [2, 'e']
-cobre['ER'] = [1,'Cu']
-cobre['PO'] =  -0.34
-cobre['PR'] = cobre['PO']*-1
-cobre['PRECO'] =  6.5 * 4 
-
-2OH = {}
-
-2OH['EOS'] = [1,'H20',1/2,'O2']
-2OH['EOE'] = [2, 'e']
-2OH['ER'] = [2,'OH']
-2OH['PO'] =  -0.40
-2OH['PR'] = 2OH['PO']*-1
-2OH['PRECO'] =  1 * 4
-
-cobre0 = {}
-
-cobre0['EOS'] = [1,'Cu']
-cobre0['EOE'] = [1, 'e']
-cobre0['ER'] = [1,'Cu']
-cobre0['PO'] =  -0.52
-cobre0['PR'] = cobre0['PO']*-1
-cobre0['PRECO'] =  6.5 * 4 
-
-iodo = {}
-
-iodo['EOS'] = [2,'I']
-iodo['EOE'] = [2, 'e']
-iodo['ER'] = [2,'I']
-iodo['PO'] =  -0.54
-iodo['PR'] = iodo['PO']*-1
-iodo['PRECO'] =  9 * 4 
+var car  {
+type:"Fiat", model:"500", color:"white"
+};
 
 
-ferro2 = {}
-
-ferro2['EOS'] = [1,'Fe']
-ferro2['EOE'] = [1, 'e']
-ferro2['ER'] = [1,'Fe']
-ferro2['PO'] =  -0.77
-ferro2['PR'] = ferro2['PO']*-1
-ferro2['PRECO'] =  2 * 4 
-
-prata = {}
-
-prata['EOS'] = [1,'Ag']
-prata['EOE'] = [1, 'e']
-prata['ER'] = [1,'Ag']
-prata['PO'] =  -0.80
-prata['PR'] = prata['PO']*-1
-prata['PRECO'] =  483 * 4 
-
-mercurio = {}
-
-mercurio['EOS'] = [1,'Hg']
-mercurio['EOE'] = [2, 'e']
-mercurio['ER'] = [1,'Hg']
-mercurio['PO'] =  -0.85
-mercurio['PR'] = mercurio['PO']*-1
-mercurio['PRECO'] =  251 * 4 
-
-bromo = {}
-
-bromo['EOS'] = [2,'Br']
-bromo['EOE'] = [2, 'e']
-bromo['ER'] = [2,'Br']
-bromo['PO'] =  -1.09
-bromo['PR'] = bromo['PO']*-1
-bromo['PRECO'] =  4.5 * 4 
-
-agua = {}
-
-agua['EOS'] = [2,'H',1/2,'O2']
-agua['EOE'] = [2, 'e']
-agua['ER'] = [1,'H2O']
-agua['PO'] =  -1.23
-agua['PR'] = agua['PO']*-1
-agua['PRECO'] =  1 * 4 
-
-cloro = {}
-
-cloro['EOS'] = [2,'Cl']
-cloro['EOE'] = [2, 'e']
-cloro['ER'] = [2,'Cl']
-cloro['PO'] =  -1.36
-cloro['PR'] = cloro['PO']*-1
-cloro['PRECO'] =  2.9 * 4 
-
-fluor = {}
-
-fluor['EOS'] = [2,'F']
-fluor['EOE'] = [2, 'e']
-fluor['ER'] = [2,'F']
-fluor['PO'] =  -2.87
-fluor['PR'] = fluor['PO']*-1
-fluor['PRECO'] =  20 * 4 
+var litio  {
+    EOS:  [1,'Li']
+    EOE:  [1, 'e']
+    ER:  Li']
+    PO:   3.04,
+    PRECO:   190 * 4
+};
 
 
+var potassio  {
+    EOS:  [1,'K']
+    EOE:  [1, 'e']
+    ER:  K']
+    PO:   2.92,
+    PRECO:   20 * 4
+};
+
+
+var bario  {
+    EOS:  [1,'Ba']
+    EOE:  [2, 'e']
+    ER:  Ba']
+    PO:   2.90,
+    PRECO:   120 * 4
+};
+
+var estroncio  {
+    EOS:  [1,'Sr']
+    EOE:  [2, 'e']
+    ER:  Sr']
+    PO:   2.89,
+    PRECO:   5.5 * 4
+};
+
+var calcio  {
+    EOS:  [1,'Ca']
+    EOE:  [2, 'e']
+    ER:  [1,'Ca']
+    PO:   2.87,
+    PRECO:   2 * 4
+};
+
+var sodio  {
+    EOS:  [1,'Na']
+    EOE:  [1, 'e']
+    ER:  [1,'Na']
+    PO:   2.71,
+    PRECO:   15 * 4
+};
+
+var magnesio  {
+    EOS:  [1,'Mg']
+    EOE:  [2, 'e']
+    ER:  [1,'Mg']
+    PO:   2.37,
+    PRECO:   14 * 4
+};
+
+var aluminio  {
+    EOS:  [1,'Al']
+    EOE:  [3, 'e']
+    ER:  [1,'Al']
+    PO:   1.66,
+    PRECO:   3.5 * 4
+};
+
+var manganes  {
+    EOS:  [1,'Mn']
+    EOE:  [2, 'e']
+    ER:  [1,'Mn']
+    PO:   1.18,
+    PRECO:   1.8 * 4
+};
+
+var H22OH  {
+    EOS:  [2,'H20']
+    EOE:  [2, 'e']
+    ER:  [2,'H',2,'OH']
+    PO:   0.83,
+    PRECO:   1 * 4
+};
+
+var zinco  {
+    EOS:  [1,'Zn']
+    EOE:  [2, 'e']
+    ER:  [1,'Zn']
+    PO:   0.76,
+    PRECO:   2.85 * 4
+};
+
+var cromio  {
+    EOS:  [1,'Cr']
+    EOE:  [3, 'e']
+    ER:  [1,'Cr']
+    PO:   0.74,
+    PRECO:   16 * 4
+};
+
+var enxofre  {
+    EOS:  [1,'S']
+    EOE:  [2, 'e']
+    ER:  [1,'S']
+    PO:   0.48,
+    PRECO:   17 * 4
+};
+
+var ferro  {
+    EOS:  [1,'Fe']
+    EOE:  [2, 'e']
+    ER:  [1,'Fe']
+    PO:   0.44,
+    PRECO:   2 * 4
+};
+
+var cobalto  {
+    EOS:  [1,'Co']
+    EOE:  [2, 'e']
+    ER:  [1,'Co']
+    PO:   0.28,
+    PRECO:   80 * 4
+};
+
+var niquel  {
+    EOS:  [1,'Ni']
+    EOE:  [2, 'e']
+    ER:  [1,'Ni']
+    PO:   0.23,
+    PRECO:   13 * 4
+};
+
+var chumbo  {
+    EOS:  [1,'Pb']
+    EOE:  [2, 'e']
+    ER:  [1,'Pb']
+    PO:   0.13,
+    PRECO:   2 * 4
+};
+
+var hidrogenio  {
+    EOS:  [2,'H']
+    EOE:  [2, 'e']
+    ER:  [2,'H']
+    PO:   0.0,
+    PRECO:   10 * 4
+};
+
+var cobre1  {
+    EOS:  [1,'Cu']
+    EOE:  [1, 'e']
+    ER:  [1,'Cu']
+    PO:   -0.15,
+    PRECO:   6.5 * 4
+};
+
+var cobre  {
+    EOS:  [1,'Cu']
+    EOE:  [2, 'e']
+    ER:  [1,'Cu']
+    PO:   -0.34,
+    PRECO:   6.5 * 4
+};
+
+var 2OH  {
+    EOS:  [1,'H20',1/2,'O2']
+    EOE:  [2, 'e']
+    ER:  [2,'OH']
+    PO:   -0.40,
+    PRECO:   1 * 4
+};
+
+var cobre0  {
+    EOS:  [1,'Cu']
+    EOE:  [1, 'e']
+    ER:  [1,'Cu']
+    PO:   -0.52,
+    PRECO:   6.5 * 4
+};
+
+var iodo  {
+    EOS:  [2,'I']
+    EOE:  [2, 'e']
+    ER:  [2,'I']
+    PO:   -0.54,
+    PRECO:   9 * 4
+};
+
+
+var ferro2  {
+    EOS:  [1,'Fe']
+    EOE:  [1, 'e']
+    ER:  [1,'Fe']
+    PO:   -0.77,
+    PRECO:   2 * 4
+};
+
+var prata  {
+    EOS:  [1,'Ag']
+    EOE:  [1, 'e']
+    ER:  [1,'Ag']
+    PO:   -0.80,
+    PRECO:   483 * 4
+};
+
+var mercurio  {
+    EOS:  [1,'Hg']
+    EOE:  [2, 'e']
+    ER:  [1,'Hg']
+    PO:   -0.85,
+    PRECO:   251 * 4
+};
+
+var bromo  {
+    EOS:  [2,'Br']
+    EOE:  [2, 'e']
+    ER:  [2,'Br']
+    PO:   -1.09,
+    PRECO:   4.5 * 4
+};
+
+var agua  {
+    EOS:  [2,'H',1/2,'O2']
+    EOE:  [2, 'e']
+    ER:  [1,'H2O']
+    PO:   -1.23,
+    PRECO:   1 * 4
+};
+
+var cloro  {
+    EOS:  [2,'Cl']
+    EOE:  [2, 'e']
+    ER:  [2,'Cl']
+    PO:   -1.36,
+    PRECO:   2.9 * 4
+};
+
+var fluor  {
+    EOS:  [2,'F']
+    EOE:  [2, 'e']
+    ER:  [2,'F']
+    PO:   -2.87,
+    PRECO:   20 * 4
+};
