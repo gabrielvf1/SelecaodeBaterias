467fcaed6568c3e7d5c6afd50aedd713b46dc3e4
Samuelgranato <samuelgranato87@gmail.com>
2019-04-22 18:23:07 -0300

    converted to js

---
+++ informacoesSubstancias.js
@@ -0,0 +1,108 @@
+var car  {
+type:"Fiat", model:"500", color:"white"
+};
+
+
+var litio = {
+    n_eletrons:  1,
+    abreviatura:  "Li",
+    potencial_reducao:   3.04,
+    preco:   190 * 4,
+    massa_molar: 5,
+
+};
+
+
+var potassio = {
+    n_eletrons:  0.5,
+    abreviatura:  "K",
+    potencial_reducao:   2.92,
+    preco:   20 * 4,
+    massa_molar: 5
+};
+
+
+var magnesio = {
+    n_eletrons:  1,
+    abreviatura:  "Mg",
+    potencial_reducao:   2.37,
+    preco:   14 * 4,
+    massa_molar: 5
+};
+
+var aluminio = {
+    n_eletrons:  1,
+    abreviatura:  "Al",
+    potencial_reducao:   1.66,
+    preco:   3.5 * 4,
+    massa_molar: 5
+};
+
+
+var zinco = {
+    n_eletrons:  1,
+    abreviatura:  "Zn",
+    potencial_reducao:   0.76,
+    preco:   2.85 * 4,
+    massa_molar: 5
+};
+
+var cromio = {
+    n_eletrons: 1,
+    abreviatura: "Cr",
+    potencial_reducao: 0.74,
+    preco:   16 * 4,
+    massa_molar: 5
+};
+
+
+var ferro = {
+    n_eletrons:  1,
+    abreviatura:  "Fe",
+    potencial_reducao:   0.44,
+    preco:   2 * 4,
+    massa_molar: 5
+};
+
+
+var niquel = {
+    n_eletrons:  1,
+    abreviatura:  "Ni",
+    potencial_reducao:   0.23,
+    preco:   13 * 4,
+    massa_molar: 5
+};
+
+var chumbo = {
+    n_eletrons:  1,
+    abreviatura:  "Pb",
+    potencial_reducao:   0.13,
+    preco:   2 * 4,
+    massa_molar: 5
+};
+
+
+var cobre = {
+    n_eletrons:  1,
+    abreviatura:  "Cu",
+    potencial_reducao:   -0.34,
+    preco:   6.5 * 4,
+    massa_molar: 5
+};
+
+
+var prata = {
+    n_eletrons:  1,
+    abreviatura:  "Ag",
+    potencial_reducao:   -0.80,
+    preco:   483 * 4,
+    massa_molar: 5
+};
+
+var mercurio = {
+    n_eletrons:  1,
+    abreviatura:  "Hg",
+    potencial_reducao:   -0.85,
+    preco:   251 * 4,
+    massa_molar: 5
+};
